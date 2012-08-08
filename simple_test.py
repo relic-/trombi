7afe7d3a17bb441ff626ff98980dc8e6c1edcb98
Igor <igor.krestov@gmail.com>
2012-08-08 20:04:39 +0000

    Added temp basic functionality testing script

---
+++ simple_test.py
@@ -0,0 +1,73 @@
+import trombi
+from tornado.ioloop import IOLoop
+
+def main():
+    main.server = trombi.Server('http://localhost:5984')
+    print('[*] Getting/ Creating db')
+    main.server.get('testdb', database_created, create=True)
+main.db = None
+main.server = None
+
+def changes(res):
+    print('~Changes', res)
+
+def database_created(db):
+    main.db = db
+    #main.db.changes(changes)
+    print('[*] Static view')
+    main.db.view('test', 'test', view_get)
+
+
+def view_get(res):
+    print(res)
+    print('[*] Temporary view')
+    mapfn = 'function(doc) { emit(null, doc);}'
+    main.db.temporary_view(temp_view, mapfn)
+
+def temp_view(res):
+    print(res)
+    print('[*] Creating document')
+    main.db.set({'testvalue': 'something', 'listattr': [1,2,3,4,5,6, {'text': 'some kind of test'}]}, doc_created)
+
+def doc_created(doc):
+    print(doc)
+    print('[*] Getting document')
+    main.db.get(doc['id'], doc_get)
+
+def doc_get(doc):
+    print(doc)
+    doc_get.doc=doc
+    print('[*] Adding attachment')
+    main.db.set_attachment(doc, 'test.txt', 'this is some random text,l o lo lo', set_attachment, type='text/plain')
+
+def set_attachment(res):
+    print(res)
+    set_attachment.att = res
+    print('[*] Getting attachment')
+    main.db.get_attachment(res['id'], 'test.txt', get_attachment)
+
+def get_attachment(res):
+    print(res)
+    print('[*] Deleting attachment')
+    main.db.delete_attachment(dict(_id=set_attachment.att['id'], _rev=set_attachment.att['rev']), 'testss.txt', del_attachment)
+
+def del_attachment(res):
+    print(res)
+    
+    print('[*] Deleting document')
+    def _del_att(res):
+        print(res)
+        delete_database()
+    main.db.delete(dict(_id=res['id'], _rev=res['rev']),_del_att)
+
+def delete_database():
+    def _del(r):
+        print(r)
+        ioloop.stop()
+    print('[*] Deleting database')
+    main.server.delete('testdb', _del)
+
+if __name__ == '__main__':
+    ioloop = IOLoop.instance()
+    ioloop.add_callback(main)
+    ioloop.start()
